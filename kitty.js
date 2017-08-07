513f11d39f29ab798f0f10c6b6bcbf45398ca840
Elvin Ramirez <real8686@gmail.com>
2017-08-06 20:21:31 -0400

    feat: add binding counter

--- kitty.js
+++ kitty.js
@@ -14,33 +14,11 @@ function binding_kitty(){
     hidding_kitty();
   }
 }
-// function Hide_kitties(){
-// 	for (var i=0; i<kitty.length; i++){
-// 		$(kitty[i]).hide();
-// 	}
-// }
-//
-// function Bind_kitties( Num_Id){
-// 	$("#button"+ Num_Id).click(function(){
-// 		Hide_kitties();
-// 		$("#kitty"+ Num_Id).show();
-// 	})
-// }
-//
-// function Bind_Counter_kitties( Num_Id){
-// 	var kitty = "#kitty"+ Num_Id
-// 	$(kitty).click(function(){
-// 		var count = $(kitty+" > .counter").text();
-// 		count = parseInt(count) + 1;
-// 		$(kitty+" > .counter").text(count);
-// 	})
-// }
-//
-//
-// for (var i=1; i<=buttons.length; i++){
-// 	Bind_Counter_kitties(i);
-// }
-//
-// for (var i=1; i<=cats.length; i++){
-// 	Bind_Counter_kitties(i);
-// }
+
+function binding_counter_kitty(){
+
+}
+
+
+hidding_kitty();
+$('#kitty').show();
