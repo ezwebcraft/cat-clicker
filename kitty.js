85338ca3d4f67d8533f118d361e37d5b9f8c3934
Elvin Ramirez <real8686@gmail.com>
2017-08-09 23:09:29 -0400

    fix: last image working now

--- kitty.js
+++ kitty.js
@@ -1,38 +1,38 @@
 var kitty = $(".kitty");
 var buttons = $("button");
 
-function hidding_kitty(){
+function hidding_kitty() {
   //hidding each kitty
-  for(var i = 0; i < kitty.length;i++){
+  for (var i = 0; i < kitty.length; i++) {
     $(kitty[i]).hide();
   }
 }
 
-function binding_kitty(num_id){
-  $("#button"+num_id).click(function(){
+function binding_kitty(num_id) {
+  $("#button" + num_id).click(function() {
     // call the hidding kitty and switch binding
     hidding_kitty();
-    $('#kitty'+ num_id).show();
+    $('#kitty' + num_id).show();
   })
 }
 
-function binding_counter_kitty(num_id){
- var kitty = "#kitty"+num_id;
- $(kitty).click(function(){
-   var count = $(kitty+" > .counter").text();
-   		count = parseInt(count) + 1;
-   		$(kitty+" > .counter").text(count);
- })
- $(kitty+" > .name").text(kitty);
+function binding_counter_kitty(num_id) {
+  var kitty = "#kitty" + num_id;
+  $(kitty).click(function() {
+    var count = $(kitty + " > .counter").text();
+    count = parseInt(count) + 1;
+    $(kitty + " > .counter").text(count);
+  })
+  $(kitty + " > .name").text(kitty);
 }
 
 // loop thu each function
 
-for(var a = 1;a<=buttons.length;a++){
+for (var a = 1; a <= buttons.length; a++) {
   binding_kitty(a);
 }
 
-for(var b = 1;b<=kitty.length;b++){
+for (var b = 1; b <= kitty.length; b++) {
   binding_counter_kitty(b);
 }
 
